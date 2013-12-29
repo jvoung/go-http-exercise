86a0c6fe18ab1238aa34b09fce5d3a76eb45e2e9
Jan Voung <jvoung@gmail.com>
2013-12-29 10:15:20 -0800

    first commit

---
+++ exercise.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  )
+
+type String string
+
+type Struct struct {
+  Greeting string
+  Punct string
+  Who string
+}
+
+func (s String) ServeHTTP(w http.ResponseWriter,
+                          r *http.Request) {
+  fmt.Fprint(w, s)
+}
+
+func (s *Struct) ServeHTTP(w http.ResponseWriter,
+                           r *http.Request) {
+  fmt.Fprintf(w, "%s%s%s\n", s.Greeting, s.Punct, s.Who)
+}
+
+func main() {
+  fmt.Println("Starting to serve from localhost:4000")
+  http.Handle("/string", String("I'm a frayed knot."))
+  http.Handle("/struct", &Struct{"Hello", ":", "Gophers!" })
+  http.ListenAndServe("localhost:4000", nil)
+}
